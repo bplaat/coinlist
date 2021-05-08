be21aa164429b104be5ad02ef4692a52507ff4a5
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2021-05-08 10:16:52 +0200

    Fixed some bugs

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ const app = new Vue({
         baseCurrency: localStorage.baseCurrency || 'USD',
 
         fetchingGlobal: false,
-        global: null,
+        global: undefined,
 
-        coinPage: localStorage.coinPage || 1,
+        coinsPage: localStorage.coinsPage || 1,
         coinsSortColumn: 'market_cap_rank',
         coinsSortDir: 'asc',
         fetchingCoins: false,
@@ -16,17 +16,18 @@ const app = new Vue({
     },
 
     watch: {
-        darkMode(newDarkMode) {
-            document.body.className = newDarkMode ? 'dark' : '';
+        darkMode(darkMode) {
+            localStorage.darkMode = darkMode;
+            document.body.className = darkMode ? 'dark' : '';
         },
 
-        baseCurrency(newBaseCurrency) {
-            localStorage.baseCurrency = newBaseCurrency;
+        baseCurrency(baseCurrency) {
+            localStorage.baseCurrency = baseCurrency;
+            this.fetchCoins();
+        },
 
-            if (this.fetchingCoins) {
-                this.fetchCoinsRequest.abort();
-                this.fetchingCoins = false;
-            }
+        coinsPage(coinsPage) {
+            localStorage.coinsPage = coinsPage;
             this.fetchCoins();
         }
     },
@@ -74,13 +75,6 @@ const app = new Vue({
     },
 
     methods: {
-        updateDarkMode(darkMode) {
-            if (this.darkMode != darkMode) {
-                this.darkMode = darkMode;
-                localStorage.darkMode = darkMode;
-            }
-        },
-
         sortCoin(column) {
             if (this.coinsSortColumn == column) {
                 this.coinsSortDir = this.coinsSortDir == 'asc' ? 'desc' : 'asc';
@@ -94,43 +88,34 @@ const app = new Vue({
             return this.coinsSortColumn == column ? (this.coinsSortDir == 'asc' ? '\u2193' : '\u2191'  ) : '';
         },
 
-        updateCoinPage(page) {
-            if (this.coinPage != page) {
-                this.coinPage = page;
-                localStorage.coinPage = page;
-
-                if (this.fetchingCoins) {
-                    this.fetchCoinsRequest.abort();
-                    this.fetchingCoins = false;
-                }
-                this.fetchCoins();
-            }
-        },
-
         fetchGlobal() {
-            if (!this.fetchingGlobal) {
-                this.fetchingGlobal = true;
-                this.fetchGlobalRequest = new XMLHttpRequest();
-                this.fetchGlobalRequest.onload = () => {
-                    this.global = JSON.parse(this.fetchGlobalRequest.responseText).data;
-                    this.fetchingGlobal = false;
-                };
-                this.fetchGlobalRequest.open('GET', 'https://api.coingecko.com/api/v3/global');
-                this.fetchGlobalRequest.send();
+            if (this.fetchingGlobal) {
+                this.fetchGlobalRequest.abort();
             }
+
+            this.fetchingGlobal = true;
+            this.fetchGlobalRequest = new XMLHttpRequest();
+            this.fetchGlobalRequest.onload = () => {
+                this.global = JSON.parse(this.fetchGlobalRequest.responseText).data;
+                this.fetchingGlobal = false;
+            };
+            this.fetchGlobalRequest.open('GET', 'https://api.coingecko.com/api/v3/global');
+            this.fetchGlobalRequest.send();
         },
 
         fetchCoins() {
-            if (!this.fetchingCoins) {
-                this.fetchingCoins = true;
-                this.fetchCoinsRequest = new XMLHttpRequest();
-                this.fetchCoinsRequest.onload = () => {
-                    this.coins = JSON.parse(this.fetchCoinsRequest.responseText);
-                    this.fetchingCoins = false;
-                };
-                this.fetchCoinsRequest.open('GET', 'https://api.coingecko.com/api/v3/coins/markets?page=' + this.coinPage + '&vs_currency=' + this.baseCurrency + '&price_change_percentage=1h,24h,7d');
-                this.fetchCoinsRequest.send();
+            if (this.fetchingCoins) {
+                this.fetchCoinsRequest.abort();
             }
+
+            this.fetchingCoins = true;
+            this.fetchCoinsRequest = new XMLHttpRequest();
+            this.fetchCoinsRequest.onload = () => {
+                this.coins = JSON.parse(this.fetchCoinsRequest.responseText);
+                this.fetchingCoins = false;
+            };
+            this.fetchCoinsRequest.open('GET', 'https://api.coingecko.com/api/v3/coins/markets?page=' + this.coinsPage + '&vs_currency=' + this.baseCurrency + '&price_change_percentage=1h,24h,7d');
+            this.fetchCoinsRequest.send();
         }
     },
 
